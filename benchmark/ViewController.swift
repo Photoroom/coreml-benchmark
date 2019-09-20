ad4587e48c6ec69a379c426120df7eaaef37951d
EliotAndres <EliotAndres@users.noreply.github.com>
2019-09-20 17:13:45 +0200

    First commit.

---
+++ benchmark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  benchmark
+//
+//  Created by Eliot Andres on 9/20/19.
+//  Copyright © 2019 Eliot Andres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
